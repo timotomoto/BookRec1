51835dfb45fb38b20a2771f9bb9c3ab3a8155668
timotomoto <100615790+timotomoto@users.noreply.github.com>
2022-07-11 16:33:22 -0400

    Added code to retrieve book data

--- bookrec.py
+++ bookrec.py
@@ -44,3 +44,22 @@ while len(selected_genre) == 0:
         select_type = str(input(
             "\nThe only matching type for the specified input is " + matching_types[0] + ". \nDo you want to look at " +
             matching_types[0] + " books? Enter y for yes and n for no\n")).lower()
+
+        if select_type == 'y':
+            selected_genre = matching_types[0]
+            print("selected_genre: " + selected_genre)
+            book_list_head = my_book_list.get_head_node()
+            # while book_list_head.get_next_node() is not None:
+            sublist_head = book_list_head.get_value().get_head_node()
+            while sublist_head.get_next_node() is not None:
+                for g in sublist_head.get_value()[0]:
+                    if g == selected_genre:
+                        print("*")
+                        print("Name: " + sublist_head.get_value()[1])
+                        print("Author: "+ sublist_head.get_value()[2])
+                        print("genre: " + str(sublist_head.get_value()[0]))
+                        print("Rating: " + sublist_head.get_value()[3])
+                        print("Year published: " + sublist_head.get_value()[4])
+                        print("*")
+                sublist_head = sublist_head.get_next_node()
+                # book_list_head = book_list_head.get_next_node()
