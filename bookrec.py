a654fd57c119396c5d9b9a9cddd0d8c3b6d641a8
timotomoto <100615790+timotomoto@users.noreply.github.com>
2022-07-11 16:22:09 -0400

    Created insert_books

--- bookrec.py
+++ bookrec.py
@@ -1,2 +1,17 @@
 from linkedlist import LinkedList
 from data import *
+
+def insert_books():
+    boocks = []
+    book_list = LinkedList()
+    for genre in genres:
+        book_sublist = LinkedList()
+        for book in books:
+            for g in book[0]:
+                if g == genre:
+                    if book not in boocks:
+                        boocks.append(book)
+    for b in boocks:
+        book_sublist.insert_beginning(b)
+    book_list.insert_beginning(book_sublist)
+    return book_list
